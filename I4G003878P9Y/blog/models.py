22bd7e2471444ed09d21ead425c519a46a5da660
Jimmy <mutabazijimmy9@gmail.com>
2022-06-23 22:00:14 +0200

    Renamed the fields to conventional standard

--- I4G003878P9Y/blog/models.py
+++ I4G003878P9Y/blog/models.py
@@ -5,11 +5,11 @@ from django.contrib.auth import get_user_model
 
 # Create your models here.
 class Post(models.Model):
-    Title=models.CharField(max_length=200)
-    Text=models.TextField()
-    Author=models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
-    Created_date=models.DateTimeField(auto_now_add=True)
-    Published_date=models.DateTimeField(auto_now_add=True)
+    title=models.CharField(max_length=200)
+    text=models.TextField()
+    author=models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    created_date=models.DateTimeField(auto_now_add=True)
+    published_date=models.DateTimeField(auto_now_add=True)
 
 
     def __str__(self):
